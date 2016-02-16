75b9d66179fd6f6ae975da77ccbfd612a3ea39b0
Gustavo Martinez <gustavo.martinez@journalism.cuny.edu>
2016-02-16 01:16:53 -0500

    added index.html, matejava.js

---
+++ matejava.js
@@ -0,0 +1,12 @@
+var a = 31;
+var b = 70;
+
+var suma = a + b;
+var resta = a - b;
+var multpilicación = a * b;
+var división = a / b;
+
+document.getElementById("c").innerHTML="La suma de " +a+ " y de " +b+ " da como resultado = " +suma;
+document.getElementById("d").innerHTML="Si " +a+ " se resta de " +b+ ", obtenemos = " +resta;
+document.getElementById("e").innerHTML="Ahora, cuando " +a+ " se multiplica con " +b+ " el producto es = " +multiplicación;
+document.getElementById("f").innerHTML="Y la división de " +a+ " entre " +b+ " tiene como solución = " +división;
